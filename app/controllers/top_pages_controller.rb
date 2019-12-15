7cae0d2c53c79c0700004a5ed04c6bc31ca1d2dc
uchidataku <bbb.rhcp99@gmail.com>
2019-12-15 20:19:57 +0900

    Top_pageの追加・Rspecの設定変更
    ・Top_pageのcontroller、home・helpページを追加
    ・Rspecの設定にcontroller_spec・helper_specを除外、request_specの作成を追記

---
+++ app/controllers/top_pages_controller.rb
@@ -0,0 +1,7 @@
+class TopPagesController < ApplicationController
+  def home
+  end
+
+  def help
+  end
+end
--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,11 @@ module Myapp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.generators do |g|
+      g.test_framework :rspec,
+                       helper_specs: false,
+                   controller_specs: false,
+                      request_specs: true
+    end
   end
 end
