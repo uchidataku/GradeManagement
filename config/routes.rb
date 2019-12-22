05c51d74f41e21c2e592274145ce73b1f415a247
uchidataku <bbb.rhcp99@gmail.com>
2019-12-22 22:12:20 +0900

    devise:users:modelsとviewsとcontrollersを生成 ・models/users.rbを生成 ・views/usersを生成 ・controllers/usersを生成 ・routes.rbにdevise forでルーティングを追記 ・homeページの新規登録とログインからリンク先を設定
    Please enter the commit message for your changes. Lines starting

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root 'top_pages#home'
   get '/help', to: 'top_pages#help'
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+         sessions: 'users/sessions',
+        passwords: 'users/passwords'
+  }
 end
