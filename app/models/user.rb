e4598fbb93c3eac491be8a1b05e603d68aa5a257
uchidataku <bbb.rhcp99@gmail.com>
2019-12-31 01:10:59 +0900

    user.rbにvalidation追加 ・名前に存在性・長さ ・emailに存在性・長さ・正規表現・一意性 ・学校に存在性・長さ ・学年に存在性

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   belongs_to :cram_schoolhouse, class_name: "CramSchoolhouse", foreign_key: "cram_schoolhouse_id"
+  validates :name, presence: true, length: { maximum: 50 }
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/ii
+  validates :email, presence: true,
+                      length: { maximum: 255 },
+                      format: { with: VALID_EMAIL_REGEX },
+                  uniqueness: { case_sensitive: false }
+  validates :school_name, presence: true, length: { maximum: 50 }
+  validates :school_year, presence: true
 end
