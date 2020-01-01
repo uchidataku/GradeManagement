24e99b535db0d2e0b67d6c52ff9ef723b7b7d77d
uchidataku <bbb.rhcp99@gmail.com>
2020-01-01 11:42:54 +0900

    railsおよびdeviseの日本語化

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,9 @@ class User < ApplicationRecord
   belongs_to :cram_schoolhouse, class_name: "CramSchoolhouse", foreign_key: "cram_schoolhouse_id"
   validates :name, presence: true, length: { maximum: 50 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/ii
-  validates :email, presence: true,
-                      length: { maximum: 255 },
-                      format: { with: VALID_EMAIL_REGEX },
-                  uniqueness: { case_sensitive: false }
+  validates :email, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                uniqueness: { case_sensitive: false }
   validates :school_name, presence: true, length: { maximum: 50 }
   validates :school_year, presence: true
 end
