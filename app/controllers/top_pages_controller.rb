ab51333b11c107ea50be37689dd9f2f2555fb664
uchidataku <bbb.rhcp99@gmail.com>
2020-01-26 15:42:32 +0900

    トップ（ログイン後）ページの実装（仮） ・仮の配色と吹き出しボックスを採用

--- app/controllers/top_pages_controller.rb
+++ app/controllers/top_pages_controller.rb
@@ -1,5 +1,8 @@
 class TopPagesController < ApplicationController
   def home
+    @user = current_user
+    @personal_achievement_test = @user.personal_achievement_tests.all
+    @report_cards = @user.report_cards.all
   end
 
   def help
