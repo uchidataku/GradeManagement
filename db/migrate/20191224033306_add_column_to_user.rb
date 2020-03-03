da9bfe75969399e0993b31bae8bf5ffcfec63d44
uchidataku <bbb.rhcp99@gmail.com>
2020-03-03 12:36:03 +0900

    migrationの修正 ・migrationの中身が辻褄が合わなくなっていたため、修正

--- db/migrate/20191224033306_add_column_to_user.rb
+++ db/migrate/20191224033306_add_column_to_user.rb
@@ -1,8 +1,8 @@
 class AddColumnToUser < ActiveRecord::Migration[5.2]
   def change
-    change_column :users, :name, :string
-    change_column :users, :school_name, :string
-    change_column :users, :shool_year, :string
-    change_column :users, :birthday, :date
+    add_column :users, :name, :string
+    add_column :users, :school_name, :string
+    add_column :users, :shool_year, :string
+    add_column :users, :birthday, :date
   end
 end
--- db/migrate/20200101030027_change_name_column_to_user.rb
+++ db/migrate/20200101030027_change_name_column_to_user.rb
@@ -1,8 +1,8 @@
 class ChangeNameColumnToUser < ActiveRecord::Migration[5.2]
   def change
-    change_column :users, :lastname_kanji, :string
-    change_column :users, :firstname_kanji, :string
-    change_column :users, :lastname_kana, :string
+    rename_column :users, :name, :lastname_kanji
+    add_column :users, :firstname_kanji, :string
+    add_column :users, :lastname_kana, :string
     add_column :users, :firstname_kana, :string
     add_column :users, :term, :string
   end
